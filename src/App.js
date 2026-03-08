f7208dec90f57674f86f3467a4b64afc305e2e15
inejih sid mhamed abderahman <inejihsidmhamed801@gmail.com>
2026-03-08 11:53:31 +0000

    done with my first +second chance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import Navbar from './navbar.js';
-
+import "./App.css";
+import Navbar from "./navbar.js";
+import Post from "./post.js";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="content">
+        <div className="posts">
+          <Post
+            title="My First Post"
+            content="This is the content of my first post."
+          />
+          <Post
+            title="My Second Post"
+            content="This is the content of my second post."
+          />
+        </div>
+        <div className="buttons">
+          <button className="custom-button">Tag Button</button>
+        </div>
+      </div>
     </div>
   );
 }
