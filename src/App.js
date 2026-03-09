ef61fb29c471457ce12dbd2fdb41efe1bb6836ad
inejih sid mhamed abderahman <inejihsidmhamed801@gmail.com>
2026-03-09 20:26:44 +0000

    starting challenge 4

--- src/App.js
+++ src/App.js
@@ -1,74 +1,33 @@
 import { useState } from 'react';
 
-const initialProducts = [{
-  id: 0,
-  name: 'Baklava',
-  count: 1,
-}, {
-  id: 1,
-  name: 'Cheese',
-  count: 5,
-}, {
-  id: 2,
-  name: 'Spaghetti',
-  count: 2,
-}];
+export default function RequestTracker() {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
 
-export default function ShoppingCart() {
-  const [
-    products,
-    setProducts
-  ] = useState(initialProducts)
-
-  function handleIncreaseClick(productId) {
-    setProducts(products.map(product => {
-      if (product.id === productId) {
-        return {
-          ...product,
-          count: product.count + 1
-        };
-      } else {
-        return product;
-      }
-    }))
-  }
-
-  function handleDecreaseClick(productId) {
-    let nextProducts = products.map(product => {
-      if (product.id === productId) {
-        return {
-          ...product,
-          count: product.count - 1
-        };
-      } else {
-        return product;
-      }
-    });
-    nextProducts = nextProducts.filter(p =>
-      p.count > 0
-    );
-    setProducts(nextProducts)
+  async function handleClick() {
+    setPending(pending + 1);
+    await delay(3000);
+    setPending(pending - 1);
+    setCompleted(completed + 1);
   }
 
   return (
-    <ul>
-      {products.map(product => (
-        <li key={product.id}>
-          {product.name}
-          {' '}
-          (<b>{product.count}</b>)
-          <button onClick={() => {
-            handleIncreaseClick(product.id);
-          }}>
-            +
-          </button>
-          <button onClick={() => {
-            handleDecreaseClick(product.id);
-          }}>
-            –
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h3>
+        Pending: {pending}
+      </h3>
+      <h3>
+        Completed: {completed}
+      </h3>
+      <button onClick={handleClick}>
+        Buy     
+      </button>
+    </>
   );
+}
+
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
